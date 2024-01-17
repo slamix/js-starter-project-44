bed7ef4464e85f82cd92817904830219add1a3d7
Сергей Баев <lokomotivsapsan@gmail.com>
2024-01-17 20:07:47 +0300

    update makefile package files and brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import func from "../scr/cli.js";
+import func from '../scr/cli.js';
 
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 func();

ac7ed710365e97d00b24feb944609451271eb471
Сергей Баев <lokomotivsapsan@gmail.com>
2024-01-19 19:54:13 +0300

    update eslintrc and fix brain-even and brain-calc

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import func from '../../scr/cli.js';
+import { printQuestion, makeQACO } from '../../scr/index.js';
 
 console.log('Welcome to the Brain Games!');
 const name = func();
 console.log(`Hello, ${name}!`);
+// makeQACO = make question, answer, compare and output
 
 const calc = (nameOfPlayer) => {
    let countOfGoodAns = 0;
@@ -13,54 +15,22 @@ const calc = (nameOfPlayer) => {
    let ansOfPlayer;
    let firstNum;
    let secondNum;
-   let result;
+   let finishOfFunc;
    console.log('What is the result of the expression?');
    while (countOfGoodAns !== 3) {
       for (let i = 0; i < 3; i++) {
-         firstNum = Math.floor(LEFT + Math.random() * (RIGHT + 1));
-         secondNum = Math.floor(LEFT + Math.random() * (RIGHT + 1));
-         if (i === 0) {
-            result = firstNum + secondNum;
-            console.log(`Question: ${firstNum} + ${secondNum}`);
-            ansOfPlayer = readlineSync.question('Your answer: ');
-            if (firstNum + secondNum === Number(ansOfPlayer)) {
-               console.log('Correct!');
-               countOfGoodAns++;
-            } else {
-               console.log(`'${ansOfPlayer}' is wrong answer ;(. Correct answer is '${result}'.`);
-               countOfGoodAns = 0;
-               console.log(`Let's try again, ${nameOfPlayer}`);
-               break;
-            }
-         } else if (i === 1) {
-            result = firstNum - secondNum;
-            console.log(`Question: ${firstNum} - ${secondNum}`);
-            ansOfPlayer = readlineSync.question('Your answer: ');
-            if (firstNum - secondNum === Number(ansOfPlayer)) {
-               console.log('Correct!');
-               countOfGoodAns++;
-            } else {
-               console.log(`'${ansOfPlayer}' is wrong answer ;(. Correct answer is '${result}'.`);
-               countOfGoodAns = 0;
-               console.log(`Let's try again, ${nameOfPlayer}`);
-               break;
-            }
+         finishOfFunc = makeQACO(i);
+         if (finishOfFunc === 'Correct!') {
+            console.log(finishOfFunc);
+            countOfGoodAns++;
          } else {
-            result = firstNum * secondNum;
-            console.log(`Question: ${firstNum} * ${secondNum}`);
-            ansOfPlayer = readlineSync.question('Your answer: ');
-            if (firstNum * secondNum === Number(ansOfPlayer)) {
-               console.log('Correct!');
-               countOfGoodAns++;
-            } else {
-               console.log(`'${ansOfPlayer}' is wrong answer ;(. Correct answer is '${result}'.`);
-               countOfGoodAns = 0;
-               console.log(`Let's try again, ${nameOfPlayer}`);
-               break;
-            }
+            countOfGoodAns = 0;
+            console.log(finishOfFunc);
+            console.log(`Let's try again, ${nameOfPlayer}`);
+            break;
          }
       }
    }
-   console.log(`Congratulation, ${nameOfPlayer}`);
+   console.log(`Congratulation, ${nameOfPlayer}!`);
 };
 calc(name);
--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import func from '../../scr/cli.js';
+import { printQuestion, makeQACO } from '../../scr/index.js';
 
 console.log('Welcome to the Brain Games!');
 const name = func();
 console.log(`Hello, ${name}!`);
+// makeQACO = make question, answer, compare and output
 
 const evenOrNot = (nameOfPlayer) => {
    console.log('Answer "yes" if the number is even, otherwise answer "no".');
@@ -13,20 +15,18 @@ const evenOrNot = (nameOfPlayer) => {
    const max = 50;
    let answer;
    let number;
+   let finishOfFunc;
    while (countOfGoodAns !== 3) {
-      number = min + Math.random() * (max + 1);
-      number = Math.floor(number);
-      console.log(`Question: ${number}`);
-      answer = readlineSync.question('Your answer: ');
-      if ((number % 2 === 0 && answer === 'yes') || (number % 2 !== 0 && answer === 'no')) {
-         console.log('Correct!');
+      finishOfFunc = makeQACO();
+      if (finishOfFunc === 'Correct!') {
+         console.log(finishOfFunc);
          countOfGoodAns++;
       } else {
          countOfGoodAns = 0;
-         console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
+         console.log(finishOfFunc);
          console.log(`Let's try again, ${nameOfPlayer}`);
       }
    }
-   console.log(`Congratulation, ${nameOfPlayer}`);
+   console.log(`Congratulation, ${nameOfPlayer}!`);
 };
 evenOrNot(name);

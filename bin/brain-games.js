8dece8af8edffb7dcbc3ca3d9747e72ba5d7d6cc
Сергей Баев <lokomotivsapsan@gmail.com>
2024-01-23 22:38:00 +0300

    rm bin/brain-games.js

--- bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import func from '../scr/cli.js';
-import evenOrNot from './brain-even.js';
-
-console.log('Welcome to the Brain Games!');
-const nameOfPlayer = func();
-console.log(`Hello, ${nameOfPlayer}`);

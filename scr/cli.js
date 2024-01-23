0f83602013becf9838dfedb157b83f6b828a2ba9
Сергей Баев <lokomotivsapsan@gmail.com>
2024-01-24 00:54:25 +0300

    fix cli.js

--- scr/cli.js
+++ scr/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 const func = () => {
-   console.log('Welcome to the Brain Games!');
    const name = readlineSync.question('May i have your name? ');
    console.log(`Hello, ${name}!`);
    return name;

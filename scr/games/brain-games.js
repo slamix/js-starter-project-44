18f44b4af74fdff573c8478a508e470eef5fa299
Сергей Баев <lokomotivsapsan@gmail.com>
2024-01-24 16:56:23 +0300

    fix error in scr/games/brain-games.js

--- scr/games/brain-games.js
+++ scr/games/brain-games.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greetings = () => {
-   const name = readlineSync.question('May i have your name: ');
+   const name = readlineSync.question('May I have your name: ');
    return name;
 };
 export default greetings;

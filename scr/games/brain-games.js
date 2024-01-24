c814c7f65dfd1d78e91f037aed578ea437a10bd9
Сергей Баев <lokomotivsapsan@gmail.com>
2024-01-24 23:32:17 +0300

    rm scr/games/brain-games.js

--- scr/games/brain-games.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const greetings = () => {
-   const name = readlineSync.question('May I have your name: ');
-   return name;
-};
-export default greetings;

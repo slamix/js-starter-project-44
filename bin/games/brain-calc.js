4621ba6ddb911978f4e982500ec3308b32b038ec
Сергей Баев <lokomotivsapsan@gmail.com>
2024-01-24 22:58:45 +0300

    fix bin/ brain-calc.js

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
 import calc from '../../scr/games/brain-calc.js';
-import func from '../../scr/cli.js';
-import greetings from '../../scr/games/brain-games.js';
 
-func();
-const name = greetings;
-console.log(`Hello, ${name}!`);
-calc(name);
+calc();

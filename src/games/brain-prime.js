1e483bb96550b43a21a616b6bf49d5e2ffe132a4
Сергей Баев <lokomotivsapsan@gmail.com>
2024-02-16 16:40:53 +0300

    fix error in brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,24 +2,24 @@ import engineOfGame from '../index.js';
 import { createNum } from '../utils.js';
 
 const isPrimeNum = () => {
-  const number = createNum(0, 100);
-  if (number === 0 || number === 1) {
+  const num = createNum(0, 50);
+  if (num === 0 || num === 1) {
     return 'no';
   }
   let d = 2;
   let result = 'yes';
-  while (d * d < number) {
-    if (number % d === 0) {
+  while (d * d < num) {
+    if (num % d === 0) {
       result = 'no';
       break;
     } else {
       d += 1;
     }
   }
-  if (d * d === number) {
+  if (d * d === num) {
     result = 'no';
   }
-  console.log(`Question: ${number}`);
+  console.log(`Question: ${num}`);
   return result;
 };
 
